12d58b2a0349aae5ffb8e260da2026659bc0118e
faisalalm01 <faisalali2858@gmail.com>
2022-04-12 11:15:32 +0700

    update project

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,31 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "back-portofolio",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "back-portofolio",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "back-portofolio",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  // "development": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "back-portofolio",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "back-portofolio",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "back-portofolio",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  development: {
+    username: "root",
+    password: null,
+    database: "portofolio",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
   use_env_variable: 'DATABASE_URL',
   dialect: "postgress",
