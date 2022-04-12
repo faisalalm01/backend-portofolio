f2a7fadd933bfc17bc1dafccd083c1827f95baf8
faisalalm01 <faisalali2858@gmail.com>
2022-04-12 10:51:07 +0700

    update project

--- src/config/database.js
+++ src/config/database.js
@@ -19,5 +19,14 @@ module.exports = {
     "database": "back-portofolio",
     "host": "127.0.0.1",
     "dialect": "mysql"
+  },
+  use_env_variable: 'DATABASE_URL',
+  dialect: "postgress",
+  protocol: "postgress",
+  dialectOptions: {
+    ssl : {
+      require : true,
+      rejectUnauthorized : false
+    }
   }
 }
