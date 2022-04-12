41784db6a9c87fd8264ef1b6544c330ef2fae990
faisalalm01 <faisalali2858@gmail.com>
2022-04-12 10:54:20 +0700

    update project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config({})
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
 const cors = require('cors')
 const mainRoutes = require('./src/routes/')
 
